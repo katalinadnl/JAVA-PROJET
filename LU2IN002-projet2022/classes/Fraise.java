6b69fc0bd547920fe91bd62f5e2ec2ef635592db
Supersaadd <96346158+Supersaadd@users.noreply.github.com>
2022-12-06 15:36:23 +0100

    test

--- LU2IN002-projet2022/classes/Fraise.java
+++ LU2IN002-projet2022/classes/Fraise.java
@@ -1,4 +1,4 @@
-public class Fraise extends Mangeable implements Mangeable{
+public class Fraise extends Mangeable implements NonToxi{
     public Fraise(int quantite){
         super("Fraise", quantite);
     }
