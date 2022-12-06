d89b7659530781ea9bd811a3e4c47524d2de14de
Supersaadd <96346158+Supersaadd@users.noreply.github.com>
2022-12-06 16:01:40 +0100

    testsim

---
+++ LU2IN002-projet2022/classes/TestSimulation.java
@@ -0,0 +1,8 @@
+public class TestSimulation{
+    public static void main(String[] args){
+        Banane b = new Banane(Math.random()*6);
+        Fraise f = new Fraise(Math.random()*6);
+        Cactus c = new Cactus(Math.random()*6);
+        
+    }
+}
